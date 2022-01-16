d86ce28dc226d99dc77bae6505846ba03b6c7f91
Zenith <93628284+Zenith4@users.noreply.github.com>
2022-01-16 12:05:29 -0600

    Update setup.py

--- zenith_deck_of_cards/setup.py
+++ zenith_deck_of_cards/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 setup(
     name="zenith_deck_of_cards",
-    version="1.1.0",
+    version="1.1.1",
     description="Deck and card classes",
     packages=["zenith_deck"],
     url="https://github.com/Zenith4/Zenith-Deck-of-Cards",    
