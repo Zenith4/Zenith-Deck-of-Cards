3f6fb19e905b319229c715881972cf4e48b68b78
Zenith <93628284+Zenith4@users.noreply.github.com>
2022-01-18 14:20:23 -0600

    Updated setup.py to prepare for update

--- zenith_deck_of_cards/setup.py
+++ zenith_deck_of_cards/setup.py
@@ -1,10 +1,13 @@
 import pathlib
 from setuptools import setup
 HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").readtext()
 setup(
     name="zenith_deck_of_cards",
-    version="1.1.2",
+    version="1.1.3",
     description="Deck and card classes",
+    long_description=README,
+    long_descrption_content_type="text/markdown",
     packages=["zenith_deck"],
     url="https://github.com/Zenith4/Zenith-Deck-of-Cards",    
 )
