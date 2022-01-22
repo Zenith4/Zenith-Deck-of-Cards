dcbbd3ae5479189fd4ce073af6dfbfd2ae7600cf
Zenith4 <zafirsahmed@gmail.com>
2022-01-21 22:22:13 -0600

    added back long_description_content_type, changed readme.md markdown, increased version from 1.2.1 to 1.2.4 during uploading process

--- zenith_deck_of_cards/setup.py
+++ zenith_deck_of_cards/setup.py
@@ -4,9 +4,10 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 setup(
     name="zenith_deck_of_cards",
-    version="1.2.1",
+    version="1.2.4",
     description="Deck and card classes",
     long_description=README,
+    long_description_content_type='text/markdown',
     author="Zenith4",
     packages=["zenith_deck"],
     url="https://github.com/Zenith4/Zenith-Deck-of-Cards",    
