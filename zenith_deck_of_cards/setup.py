f92032db23870a0b49bb2d5dd973e4a89d21508a
Zenith4 <zafirsahmed@gmail.com>
2022-01-21 21:14:52 -0600

    Removed long_description_content_type, changed readtext to read_text

--- zenith_deck_of_cards/setup.py
+++ zenith_deck_of_cards/setup.py
@@ -1,13 +1,12 @@
 import pathlib
 from setuptools import setup
 HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").readtext()
+README = (HERE / "README.md").read_text()
 setup(
     name="zenith_deck_of_cards",
     version="1.2.0",
     description="Deck and card classes",
     long_description=README,
-    long_descrption_content_type="text/markdown",
     author="Zenith4",
     packages=["zenith_deck"],
     url="https://github.com/Zenith4/Zenith-Deck-of-Cards",    
